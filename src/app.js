d9b28cdc800cc66f438cf6e79eac4468c4ba7710
KanekiPrograming <54086856+KanekiPrograming@users.noreply.github.com>
2020-06-19 14:28:20 -0500

    firstcommit

---
+++ src/app.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+function App(){
+  return(
+    <p>hola mundo</p>
+  );
+}
+////////////////////////////////////////////
+export default App;
